e8be6432cfff236dcdb6f503975e6128cdbc1ead
Keiko <keikonagami3@gmail.com>
2019-08-15 02:20:50 -0700

    take measurements while flying

--- baselines/baseline_racer_perception.py
+++ baselines/baseline_racer_perception.py
@@ -28,6 +28,8 @@ class BaselineRacerPerception(BaselineRacer):
         self.waypoint_gate_1 = np.array([0.0,0.0,+1.0,1.0]).T.reshape(4,1)
         self.waypoint_gate_2 = np.array([0.0,0.0,0.0,1.0]).T.reshape(4,1)
         self.waypoint_gate_3 = np.array([0,0.0,-2.0,1.0]).T.reshape(4,1)
+        self.waypoint_gate_4 = np.array([0,0.0,-6.0,1.0]).T.reshape(4,1)
+        self.waypoint_gate_5 = np.array([0,0.0,-20.0,1.0]).T.reshape(4,1)
         self.objpoints = 1.5*np.array([[0,0,0],[-1.0,1.0,0],[1.0,1.0,0],[1.0,-1.0,0],[-1.0,-1.0,0]])
 
     # Find area of polygon
@@ -73,30 +75,40 @@ class BaselineRacerPerception(BaselineRacer):
                 large_aspect_ratio = aspect_ratio_mtx[0,i]
         return large_aspect_ratio
 
+    def get_rotation_matrix_quad_frame_to_global_frame(self,state):
+        q0 = state.orientation.w_val
+        q1 = state.orientation.x_val
+        q2 = state.orientation.y_val
+        q3 = state.orientation.z_val
+
+        # rotation matrix between quad body frame and global frame
+        rt_mtx_quad2global = np.array([[1-2*(q2**2+q3**2), 2*(q1*q2-q0*q3), 2*(q1*q3-q0*q2)],
+                                        [2*(q1*q2 + q0*q3), 1-2*(q1**2+q3**2), 2*(q2*q3-q0*q1)],
+                                        [2*(q1*q3-q0*q2), 2*(q1*q0+q2*q3), 1-2*(q1**2+q2**2)]])
+        return rt_mtx_quad2global
+
+    def gate_frame_waypoint_to_global_waypoint(self,state,rt_mtx_gate2quad,rt_mtx_quad2global,waypoint_gate):
+        waypoint_rel = np.dot(rt_mtx_gate2quad, waypoint_gate)
+        waypoint_rel = np.dot(rt_mtx_quad2global, np.array([waypoint_rel[2], waypoint_rel[0], -waypoint_rel[1]]))
+        waypoint_glob = np.reshape(np.array([state.position.x_val + waypoint_rel[0], state.position.y_val + waypoint_rel[1], state.position.z_val - waypoint_rel[2]]),(1,3))
+        return waypoint_glob
+
     def run(self):
         # Move through first gate
         self.get_ground_truth_gate_poses()
         #self.airsim_client.plot_transform([self.gate_poses_ground_truth[0]], vehicle_name=self.drone_name)
         self.airsim_client.moveOnSplineAsync([self.gate_poses_ground_truth[0].position], vel_max = 2.0, acc_max = 5.0, viz_traj=self.viz_traj, vehicle_name=self.drone_name)
         time.sleep(2)
-        print(self.gate_poses_ground_truth[1])
+        waypoint_2 = airsim.Vector3r(0,10,self.gate_poses_ground_truth[0].position.z_val)
         while self.airsim_client.isApiControlEnabled(vehicle_name=self.drone_name):
             # Take image
             response = self.airsim_client.simGetImages([airsim.ImageRequest("fpv_cam", airsim.ImageType.Scene, False, False)])
             img1d = np.fromstring(response[0].image_data_uint8, dtype=np.uint8) # get numpy array
             image_rgb = img1d.reshape(response[0].height, response[0].width, 3)
 
-            # Get quad state when the image was taken
+            # get rotation matrix from quad frame to global frame
             state = self.airsim_client.simGetVehiclePose()
-            q0 = state.orientation.w_val
-            q1 = state.orientation.x_val
-            q2 = state.orientation.y_val
-            q3 = state.orientation.z_val
-
-            # rotation matrix between quad body frame and global frame
-            rt_mtx_quad2global = np.array([[1-2*(q2**2+q3**2), 2*(q1*q2-q0*q3), 2*(q1*q3-q0*q2)],
-                                            [2*(q1*q2 + q0*q3), 1-2*(q1**2+q3**2), 2*(q2*q3-q0*q1)],
-                                            [2*(q1*q3-q0*q2), 2*(q1*q0+q2*q3), 1-2*(q1**2+q2**2)]])
+            rt_mtx_quad2global = self.get_rotation_matrix_quad_frame_to_global_frame(state)
 
             # Find corners of the gate
             mask = cv2.inRange(image_rgb,self.lower_green,self.upper_green)
@@ -109,6 +121,7 @@ class BaselineRacerPerception(BaselineRacer):
             cv2.imshow("dilated_gate", dilated_gate)
             cv2.imshow("eroded_gate", eroded_gate)
             cv2.waitKey(1)
+
             largest_area = 0.0
             gate_corners = None
             gate_corners_best = np.zeros((1,3))
@@ -128,13 +141,12 @@ class BaselineRacerPerception(BaselineRacer):
                             gate_corners_best = gate_corners
 
             # MOVE
-            if largest_area < 500 or len(gate_corners_best)!=4:
+            if largest_area < 800 or len(gate_corners_best)!=4:
                 self.no_gate_count += 1
-                #self.wrong_gate_count = 0
                 print("Gate NOT detected")
-                # If no gate has been detected for over 25 attempts, rotate to look for gate
-                if self.no_gate_count > 25:
-                    self.airsim_client.moveByYawRateAsync(yaw_rate=-15.0, duration=2, vehicle_name=self.drone_name) # rotate counter clockwise to look for next gate
+                # If no gate has been detected for over 50 attempts, rotate to look for gate
+                if self.no_gate_count > 50:
+                    self.airsim_client.moveByYawRateAsync(yaw_rate=-15.0, duration=0.05, vehicle_name=self.drone_name).join() # rotate counter clockwise to look for next gate
 
             else:
                 self.no_gate_count = 0
@@ -153,54 +165,45 @@ class BaselineRacerPerception(BaselineRacer):
                 imgpoints = np.float32(np.concatenate((np.array(gate_center_pixel).reshape(-1,1,2),src_pts),axis=0))
 
                 # find rotation/translation matrix between image to gate centered frame
-                ret, rvecs, tvecs = cv2.solvePnP(self.objpoints,imgpoints,self.camera_matrix,self.dist_coeff)
-                rvec_full, jac1 = cv2.Rodrigues(rvecs)
-                rt_mtx_gate2quad = np.concatenate((rvec_full, tvecs), axis=1)
+                ret, rvec, tvec = cv2.solvePnP(self.objpoints,imgpoints,self.camera_matrix,self.dist_coeff)
+                rvec_full, jac1 = cv2.Rodrigues(rvec)
+                rt_mtx_gate2quad = np.concatenate((rvec_full, tvec), axis=1)
                 rt_mtx_gate2quad = np.concatenate((rt_mtx_gate2quad, np.array([0,0,0,1]).reshape(1,4)), axis=0)
 
-                # relative location of gate from quad to global location of gate
-                waypoint_rel_1 = np.dot(rt_mtx_gate2quad, self.waypoint_gate_1)
-                waypoint_rel_1 = np.dot(rt_mtx_quad2global, np.array([waypoint_rel_1[2], waypoint_rel_1[0], -waypoint_rel_1[1]]))
+                # gate frame waypoint to global frame waypoint
+                waypoint_glob_1 = self.gate_frame_waypoint_to_global_waypoint(state,rt_mtx_gate2quad,rt_mtx_quad2global,self.waypoint_gate_1)
+                waypoint_glob_2 = self.gate_frame_waypoint_to_global_waypoint(state,rt_mtx_gate2quad,rt_mtx_quad2global,self.waypoint_gate_2)
+                waypoint_glob_3 = self.gate_frame_waypoint_to_global_waypoint(state,rt_mtx_gate2quad,rt_mtx_quad2global,self.waypoint_gate_3)
+                waypoint_glob_4 = self.gate_frame_waypoint_to_global_waypoint(state,rt_mtx_gate2quad,rt_mtx_quad2global,self.waypoint_gate_4)
+                waypoint_glob_5 = self.gate_frame_waypoint_to_global_waypoint(state,rt_mtx_gate2quad,rt_mtx_quad2global,self.waypoint_gate_5)
 
-                waypoint_rel_2 = np.dot(rt_mtx_gate2quad, self.waypoint_gate_2)
-                waypoint_rel_2 = np.dot(rt_mtx_quad2global, np.array([waypoint_rel_2[2], waypoint_rel_2[0], -waypoint_rel_2[1]]))
-
-                waypoint_rel_3 = np.dot(rt_mtx_gate2quad,self.waypoint_gate_3)
-                waypoint_rel_3 = np.dot(rt_mtx_quad2global, np.array([waypoint_rel_3[2], waypoint_rel_3[0], -waypoint_rel_3[1]]))
-
-                waypoint_glob_1 = np.reshape(np.array([state.position.x_val + waypoint_rel_1[0], state.position.y_val + waypoint_rel_1[1], state.position.z_val - waypoint_rel_1[2]]),(1,3))
-                waypoint_glob_2 = np.reshape(np.array([state.position.x_val + waypoint_rel_2[0], state.position.y_val + waypoint_rel_2[1], state.position.z_val - waypoint_rel_2[2]]),(1,3))
-                waypoint_glob_3 = np.reshape(np.array([state.position.x_val + waypoint_rel_3[0], state.position.y_val + waypoint_rel_3[1], state.position.z_val - waypoint_rel_3[2]]),(1,3))
-
-                # if quad is too close to next waypoint, move through gate
-                self.quad_dist_error = abs(np.linalg.norm(np.array([state.position.x_val,state.position.y_val,state.position.z_val]) - self.waypoint_ave_2))
-                if self.quad_dist_error < 3.0 and self.close_count == 0:
+                # if quad is too close to next waypoint, move through gate without taking measurements
+                quad_to_next_gate_dist = abs(np.linalg.norm(np.array([state.position.x_val,state.position.y_val,state.position.z_val]) - self.waypoint_ave_2))
+                if quad_to_next_gate_dist < 3.0 and self.close_count == 0:
                     print("too close to gate")
                     self.airsim_client.moveOnSplineAsync([waypoint_3], vel_max = 2.0, acc_max = 2.0, add_curr_odom_position_constraint=True, add_curr_odom_velocity_constraint=True, viz_traj=self.viz_traj, vehicle_name=self.drone_name)
-                    time.sleep(3)
+                    time.sleep(2)
                     self.measurement_count = 0
                     self.wrong_gate_count = 0
-                    self.close_count += 1
+                    self.close_count = 1
                     self.waypoint_ave_2 = copy.deepcopy(waypoint_glob_2)
                 else:
                     if self.close_count == 1: # right after passing through gate, reinitialize
                         self.waypoint_ave_2 = copy.deepcopy(waypoint_glob_2)
                         self.measurement_count = 0
                     self.close_count = 0
-                    norm_dist_error = abs(np.linalg.norm(waypoint_glob_2-self.waypoint_ave_2))
-                    quad_meas_error = abs(np.linalg.norm(waypoint_glob_2-np.array([state.position.x_val,state.position.y_val,state.position.z_val])))
-                    # if new measurement is very far from previous measurement, wrong gate is measured
-                    if norm_dist_error > 1.0:
-                        print(norm_dist_error, quad_meas_error)
+                    measurement_diff = abs(np.linalg.norm(waypoint_glob_2-self.waypoint_ave_2))
+                    quad_to_measurement_dist = abs(np.linalg.norm(waypoint_glob_2-np.array([state.position.x_val,state.position.y_val,state.position.z_val])))
+
+                    if measurement_diff > 0.5: # if new measurement is very far from previous measurements, wrong gate is measured
+                        print(measurement_diff, quad_to_measurement_dist)
                         print("wrong gate", self.wrong_gate_count)
-                        # if wrong gate is measured over 10 times, reinitialize next waypoint
-                        if self.wrong_gate_count > 10:
+                        if self.wrong_gate_count > 10: # if wrong gate is measured over 10 times, reinitialize next waypoint
                             self.measurement_count = 0
                             self.waypoint_ave_2 = copy.deepcopy(waypoint_glob_2)
                         self.wrong_gate_count += 1
-                    # if new measurement is very far from quad, wrong gate is measured
-                    elif quad_meas_error > 20:
-                        print(norm_dist_error, quad_meas_error)
+                    elif quad_to_measurement_dist > 20: # if new measurement is very far from quad, wrong gate is measured
+                        print(measurement_diff, quad_to_measurement_dist)
                         print("wrong gate", self.wrong_gate_count)
                     else:
                         cv2.circle(image_rgb, (int(gate_center_pixel[0]), int(gate_center_pixel[1])), 10, (255, 0, 0), -1)
@@ -214,28 +217,32 @@ class BaselineRacerPerception(BaselineRacer):
                         self.measurement_count += 1
                         print("Gate detected, Measurement Taken")
                         print(self.measurement_count)
+
                         # calculate moving average of gate waypoints
                         if self.measurement_count == 1:
-                            measurement_estimates_mtx_1 = np.reshape(waypoint_glob_1,(1,3))
-                            measurement_estimates_mtx_2 = np.reshape(waypoint_glob_2,(1,3))
-                            measurement_estimates_mtx_3 = np.reshape(waypoint_glob_3,(1,3))
-                            waypoint_ave_1 = np.reshape(waypoint_glob_1,(1,3))
-                            self.waypoint_ave_2 = np.reshape(waypoint_glob_2,(1,3))
-                            waypoint_ave_3 = np.reshape(waypoint_glob_3,(1,3))
-                        else:
-                            measurement_estimates_mtx_1 = np.append(measurement_estimates_mtx_1, waypoint_glob_1,axis=0)
-                            measurement_estimates_mtx_2 = np.append(measurement_estimates_mtx_2, waypoint_glob_2,axis=0)
-                            measurement_estimates_mtx_3 = np.append(measurement_estimates_mtx_3, waypoint_glob_3,axis=0)
-                            waypoint_ave_1 = np.reshape(np.mean(measurement_estimates_mtx_1,axis=0),(1,3))
-                            self.waypoint_ave_2 = np.reshape(np.mean(measurement_estimates_mtx_2,axis=0),(1,3))
-                            waypoint_ave_3 = np.reshape(np.mean(measurement_estimates_mtx_3,axis=0),(1,3))
+                            measurement_estimates_mtx_1 = np.empty((0,3))
+                            measurement_estimates_mtx_2 = np.empty((0,3))
+                            measurement_estimates_mtx_3 = np.empty((0,3))
+                            measurement_estimates_mtx_4 = np.empty((0,3))
+                            measurement_estimates_mtx_5 = np.empty((0,3))
+                        measurement_estimates_mtx_1 = np.append(measurement_estimates_mtx_1, waypoint_glob_1,axis=0)
+                        measurement_estimates_mtx_2 = np.append(measurement_estimates_mtx_2, waypoint_glob_2,axis=0)
+                        measurement_estimates_mtx_3 = np.append(measurement_estimates_mtx_3, waypoint_glob_3,axis=0)
+                        measurement_estimates_mtx_4 = np.append(measurement_estimates_mtx_4, waypoint_glob_4,axis=0)
+                        measurement_estimates_mtx_5 = np.append(measurement_estimates_mtx_5, waypoint_glob_5,axis=0)
+                        waypoint_ave_1 = np.reshape(np.mean(measurement_estimates_mtx_1,axis=0),(1,3))
+                        self.waypoint_ave_2 = np.reshape(np.mean(measurement_estimates_mtx_2,axis=0),(1,3))
+                        waypoint_ave_3 = np.reshape(np.mean(measurement_estimates_mtx_3,axis=0),(1,3))
+                        waypoint_ave_4 = np.reshape(np.mean(measurement_estimates_mtx_4,axis=0),(1,3))
+                        waypoint_ave_5 = np.reshape(np.mean(measurement_estimates_mtx_5,axis=0),(1,3))
 
-                        # three waypoints
+                        # waypoints
                         waypoint_1 = airsim.Vector3r(waypoint_ave_1[0,0],waypoint_ave_1[0,1], waypoint_ave_1[0,2])
                         waypoint_2 = airsim.Vector3r(self.waypoint_ave_2[0,0],self.waypoint_ave_2[0,1], self.waypoint_ave_2[0,2])
                         waypoint_3 = airsim.Vector3r(waypoint_ave_3[0,0],waypoint_ave_3[0,1], waypoint_ave_3[0,2])
+                        waypoint_4 = airsim.Vector3r(waypoint_ave_4[0,0],waypoint_ave_4[0,1], waypoint_ave_4[0,2])
+                        waypoint_5 = airsim.Vector3r(waypoint_ave_5[0,0],waypoint_ave_5[0,1], waypoint_ave_5[0,2])
 
-                        #self.airsim_client.moveOnSplineAsync([waypoint_1,waypoint_2,waypoint_3], vel_max = 2.0, acc_max = 2.0, add_curr_odom_position_constraint=True, add_curr_odom_velocity_constraint=True, viz_traj=self.viz_traj, vehicle_name=self.drone_name)
                         self.airsim_client.moveOnSplineAsync([waypoint_2], vel_max = 2.0, acc_max = 2.0, add_curr_odom_position_constraint=True, add_curr_odom_velocity_constraint=True, viz_traj=self.viz_traj, vehicle_name=self.drone_name)
 
 def main(args):
