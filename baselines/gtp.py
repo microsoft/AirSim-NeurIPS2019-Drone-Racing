37d7e92670f096c67774f873faff5b9d919d288d
Tim Taubner <taubnert@ethz.ch>
2019-08-21 16:19:35 -0700

    CIP: Comments in Progress

--- baselines/gtp.py
+++ baselines/gtp.py
@@ -12,16 +12,15 @@ gate_facing_vector = airsim.Vector3r(x_val=0, y_val=1, z_val=0)
 def rotate_vector(q, v):
     v_quat = v.to_Quaternionr()
     v_rotated_ = q * v_quat * q.inverse()
-    # v_rotated_ = v_.rotate(q)  # seems like it's a rotate by operator
     return airsim.Vector3r(x_val=v_rotated_.x_val, y_val=v_rotated_.y_val, z_val=v_rotated_.z_val)
 
 
-"""
-This class represents a Track defined by Gates.
-A spline is fitted through the Gates with tangential constraints.
-This is then resampled to give smooth tangents and normals  
-"""
 class SplinedTrack:
+    """This class represents a Track defined by Gates.
+    A spline is fitted through the Gates with tangential constraints.
+    This spline is then sampled at 2048 points.
+    """
+
     def __init__(self, gate_poses):
         self.gates = gate_poses
 
@@ -62,105 +61,136 @@ class SplinedTrack:
         self.track_widths = self.track_width_spline(taus)
         self.track_heights = self.track_height_spline(taus)
 
-    # Find closest re-sampled point
-    # In theory, could find closest point on Spline directly,
-    # see e.g. https://computingandrecording.wordpress.com/2017/03/20/closest-point-to-a-cubic-spline/
-    # returns idx of point, center, tangent and normal
     def track_frame_at(self, p):
+        """Find closest track frame to a reference point p.
+        :param p: Point of reference
+        :return: Index of track frame, track center, tangent and normal.
+        """
         i = np.linalg.norm(self.track_centers - p, axis=1).argmin()
         return i, self.track_centers[i], self.track_tangents[i], self.track_normals[i], \
                self.track_widths[i], self.track_heights[i]
 
 
-# ==== OVERVIEW ====
-# Given the state of both drones 0 and 1, this controller iteratively computes trajectories for both drones, that are
-# collision-free and stay within the track and conform to the dynamical model (maximal speed).
-# These trajectories are specified as 'n_steps' points every 'dt' seconds.
-#
-# ==== ITERATIVE BEST RESPONSE ====
-# Initially, these trajectories are sampled to follow the track (for more information on the track see above).
-# Then, fixing the trajectory for drone 1, the trajectory for drone 0 is optimized.
-# Next, vice versa, the trajectory for drone 1 is optimized while keeping the trajectory for drone 0 fixed.
-# This is done iteratively until either the change becomes small, or a fixed number of iterations is reached.
-# The hope is that eventually we arrive at a fixed-point, i. e. after optimizing for both drones we get the same
-# trajectories again.
-#
-# ==== COMPUTING THE BEST RESPONSE ====
-# Since some of the constraints are non-convex quadratic constraint (namely the non-collision constraint),
-# the optimized trajectory is found in an iteratively fashion (sequential quadratic program, short SQP) by linearizing
-# the non-collision constraints around the current guess.
-#
-# ==== PARAMETERS ====
-# i_ego         The index of the 'ego' drone
-# i_opp         The index of the opponent drone
-# dt            Sampling time for trajectories
-# n_steps       Length of the trajectories
-# n_game_iters  Number of game iterations (how often the best response is computed for drone i_0)
-# n_sqp_iters   Number of SQP iterations (how often the constraints are linearized and the optimization is solved)
-# drone_params
-#   r_coll      Collision radius, see competition guidelines.
-#   r_safe      Safety radius, see competition guidelines
-#   v_max       Maximal velocity, determines how far waypoints can be apart
-#   a_max       Maximal acceleration, not used here.
 class Controller:
+    """
+    OVERVIEW:
+    Given the state of both drones 0 and 1, this controller iteratively computes trajectories for both drones, that are
+    collision-free and stay within the track and conform to the dynamical model (maximal speed).
+    These trajectories are specified as 'n_steps' points every 'dt' seconds.
+
+    ITERATIVE BEST RESPONSE:
+    Initially, these trajectories are sampled to follow the track (for more information on the track see above).
+    Then, fixing the trajectory for drone 1, the trajectory for drone 0 is optimized.
+    Next, vice versa, the trajectory for drone 1 is optimized while keeping the trajectory for drone 0 fixed.
+    This is done iteratively until either the change becomes small, or a fixed number of iterations is reached.
+    The hope is that eventually we arrive at a fixed-point, i. e. after optimizing for both drones we get the same
+    trajectories again.
+
+    COMPUTING THE BEST RESPONSE:
+    Since some of the constraints are non-convex quadratic constraint (namely the non-collision constraint),
+    the optimized trajectory is found in an iteratively fashion (sequential quadratic program, short SQP) by linearizing
+    the non-collision constraints around the current guess.
+    In case no feasible solution is found, the problem is relaxed by turning constraints into objectives.
+
+    PARAMETERS:
+     i_ego         The index of the 'ego' drone
+     i_opp         The index of the opponent drone
+     dt            Sampling time for trajectories
+     n_steps       Length of the trajectories
+     n_game_iters  Number of game iterations (how often the best response is computed for drone i_0)
+     n_sqp_iters   Number of SQP iterations (how often the constraints are linearized and the optimization is solved)
+     drone_params
+       r_coll      Collision radius, see competition guidelines.
+       r_safe      Safety radius, see competition guidelines
+       v_max       Maximal velocity, determines how far waypoints can be apart
+       a_max       Maximal acceleration, not used here.
+    """
+
     def __init__(self, params, drone_params, gate_poses):
         self.dt = params.dt
         self.n_steps = params.n
         self.drone_params = drone_params
-
         self.track = SplinedTrack(gate_poses)
 
-    # Based on the start position p_0, return an initial guess for a trajectory
-    # This is simply a line following the tangent
+        # These are some parameters that could be tuned.
+        # They control how the safety penalty and the relaxed constraints are weighted.
+        self.nc_weight = 2.0
+        self.nc_relax_weight = 128.0
+        self.track_relax_weight = 16.0
+
     def init_trajectory(self, i_0, p_0):
-        N = self.n_steps
+        """Initialize Trajectory along the track tangent
+        Based on the start position p_0, return an initial guess for a trajectory
+        This is simply a line following the tangent of the track with maximal speed
+
+        :param i_0: The index of the drone to initialize the trajectory for.
+        :param p_0: The current position of the drone
+        :return: A trajectory of length self.n_steps
+        """
         v_ego = self.drone_params[i_0]["v_max"]
-        trajectory = np.zeros(shape=(N, 3))
+        trajectory = np.zeros(shape=(self.n_steps, 3))
         p = np.copy(p_0)  # Copy state as it gets modified throughout the loop
-        for i in range(N):
+        for i in range(self.n_steps):
             idx, c, t, n, width, height = self.track.track_frame_at(p)
             p += self.dt * v_ego * t
             trajectory[i, :] = p
         return trajectory
 
-    # Based on current trajectories, compute the best-response (BR) of player i,
-    # and return it.
-    def best_response(self, i_0, state, trajectories):
-        i_1 = (i_0 + 1) % 2
-        v_ego = self.drone_params[i_0]["v_max"]
-        r_coll_ego = self.drone_params[i_0]["r_coll"]
-        r_coll_opp = self.drone_params[i_1]["r_coll"]
-        r_safe_ego = self.drone_params[i_0]["r_safe"]
-        r_safe_opp = self.drone_params[i_1]["r_safe"]
+    def best_response(self, i_ego, state, trajectories):
+        """Based on current trajectories, compute the best-response (BR) of player i_ego.
 
-        # These are some parameters that could be tuned.
-        # They control how the safety penalty and the relaxed constraints are weighted.
-        nc_weight = 2.0
-        nc_relax_weight = 128.0
-        track_relax_weight = 16.0
+        This is done by solving an optimization problem over the trajectory
+          p_ego[0], p_ego[1], ..., p_ego[N]
+        for drone i_ego maximizing the progress along the track,
+        subject to
+          - dynamical constraints
+            ||p_ego[k+1] - p_ego[k]|| <= v_max*dt
+          - stay-within-track constraints
+                  |n'(p[k] - c)| <= width,
+             |[0 0 1](p[k] - c)| <= height,
+            where n is the track normal (a vector on the xy-plane pointing perpendicular to the track tangent),
+              and c is the track center
+          - non-collision constraints
+            ||p_ego[k] - p_opp[k]|| >= r_coll * 2
 
-        N = self.n_steps
-        p = cp.Variable(shape=(N, 3))
+        The progress along the track is approximated by the progress along the tangent
+        of the last point of the trajectory, i. e. t'p[k].
+        The non-collision constraints are non convex and are hence linearized. Instead of requiring the ego drone to
+        stay outside a circle, the drone is constrained to be in a half-plane tangential to that circle.
+        In addition to optimizing track progress, the penalty of violating the safety radius is accounted for.
+
+        :param i_ego: The drone index of the ego drone.
+        :param state: The current state (positions) of the two drones.
+        :param trajectories: The current trajectories
+        :return: Optimized trajectory of player i_ego
+        """
+        i_opp = (i_ego + 1) % 2
+        v_ego = self.drone_params[i_ego]["v_max"]
+        r_coll_ego = self.drone_params[i_ego]["r_coll"]
+        r_coll_opp = self.drone_params[i_opp]["r_coll"]
+        r_safe_ego = self.drone_params[i_ego]["r_safe"]
+        r_safe_opp = self.drone_params[i_opp]["r_safe"]
+
+        p = cp.Variable(shape=(self.n_steps, 3))
 
         # Dynamical Constraints:
         # ||p_0 - p[0]|| <= v*dt
-        init_dyn_constraint = cp.SOC(cp.Constant(v_ego * self.dt), cp.Constant(state[i_0, :]) - p[0, :])
+        init_dyn_constraint = cp.SOC(cp.Constant(v_ego * self.dt), cp.Constant(state[i_ego, :]) - p[0, :])
 
         # ||p[k+1] - p[k]|| <= v*dt
         dyn_constraints = [init_dyn_constraint] + [
-            cp.SOC(cp.Constant(v_ego * self.dt), p[k + 1, :] - p[k, :]) for k in range(N - 1)]
+            cp.SOC(cp.Constant(v_ego * self.dt), p[k + 1, :] - p[k, :]) for k in range(self.n_steps - 1)]
 
         track_constraints = []
         track_obj = cp.Constant(0)
         # exponentially decreasing weight
         track_objective_exp = 0.5
 
-        for k in range(N):
-            idx, c, t, n, width, height = \
-                self.track.track_frame_at(trajectories[i_0][k, :])
+        for k in range(self.n_steps):
+            idx, c, t, n, width, height = self.track.track_frame_at(trajectories[i_ego][k, :])
             track_constraints.append(n.T @ p[k, :] - n.dot(c) <= width - r_coll_ego)
             track_constraints.append(n.T @ p[k, :] - n.dot(c) >= -(width - r_coll_ego))
-            # 3D Height. This assumes non rotated gates.
+            # 3D Height. This assumes that gates are not rolled (i. e. aligned with the z-axis)
             track_constraints.append(p[k, 2] - c[2] <= height - r_coll_ego)
             track_constraints.append(p[k, 2] - c[2] >= -(height - r_coll_ego))
 
@@ -174,13 +204,12 @@ class Controller:
         # exponentially decreasing weight
         non_collision_objective_exp = 0.5
 
-        for k in range(N):
-            p_opp = trajectories[i_1][k, :]
-            p_ego = trajectories[i_0][k, :]
+        for k in range(self.n_steps):
+            p_opp = trajectories[i_opp][k, :]
+            p_ego = trajectories[i_ego][k, :]
             beta = p_opp - p_ego
-            # TODO: Fix this.
-            assert np.linalg.norm(beta) >= 1e-6
-            beta /= np.linalg.norm(beta)
+            if np.linalg.norm(beta) >= 1e-5:
+                beta /= np.linalg.norm(beta)
             #     n.T * (p_opp - p_ego) >= r_coll_opp + r_coll_ego
             non_collision_constraints.append(beta.dot(p_opp) - beta.T @ p[k, :] >= r_coll_opp + r_coll_ego)
             # <=> -n.T * p_ego >= r_coll_opp + r_coll_ego - n.T * p_opp
@@ -193,9 +222,11 @@ class Controller:
             nc_relax_obj += (non_collision_objective_exp ** k) * cp.pos(
                 (r_coll_opp + r_coll_ego) - (beta.dot(p_opp) - beta.T @ p[k, :]))
 
-        _, _, t, _, _, _ = self.track.track_frame_at(trajectories[i_0][-1, :])
+        # Take the tangent t at the last trajectory point
+        # This serves
+        _, _, t, _, _, _ = self.track.track_frame_at(trajectories[i_ego][-1, :])
         obj = -t.T @ p[-1, :]
-        prob = cp.Problem(cp.Minimize(obj + nc_weight * nc_obj),
+        prob = cp.Problem(cp.Minimize(obj + self.nc_weight * nc_obj),
                           dyn_constraints + track_constraints + non_collision_constraints)
         prob.solve()
 
@@ -206,7 +237,7 @@ class Controller:
             assert prob.value >= 0.0
 
             # Solve relaxed problem (non-collision constraint -> non-collision objective)
-            relaxed_prob = cp.Problem(cp.Minimize(obj + nc_weight * nc_obj + track_relax_weight * track_obj),
+            relaxed_prob = cp.Problem(cp.Minimize(obj + self.nc_weight * nc_obj + self.track_relax_weight * track_obj),
                                       dyn_constraints + non_collision_constraints)
             relaxed_prob.solve()
 
@@ -217,7 +248,7 @@ class Controller:
                 assert prob.value >= 0.0
 
                 # Solve relaxed problem (non-collision constraint -> non-collision objective)
-                relaxed_prob = cp.Problem(cp.Minimize(obj + nc_weight * nc_obj + nc_relax_weight * nc_relax_obj),
+                relaxed_prob = cp.Problem(cp.Minimize(obj + self.nc_weight * nc_obj + self.nc_relax_weight * nc_relax_obj),
                                           dyn_constraints + track_constraints)
                 relaxed_prob.solve()
 
